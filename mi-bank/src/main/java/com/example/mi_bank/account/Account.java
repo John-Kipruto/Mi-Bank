69614ce89e011a4ad0bd77fb87aca61b2700a84b
John Kipruto <johnkipruto157@gmail.com>
2024-06-16 19:24:58 +0300

    Initial commit

---
+++ mi-bank/src/main/java/com/example/mi_bank/account/Account.java
@@ -0,0 +1,37 @@
+package com.example.mi_bank.account;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ mi-bank/src/test/java/com/example/mi_bank/MiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mi_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
